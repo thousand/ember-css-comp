0f4aff6ff10de9809397ceeadc290dc78ee92909
Bryan Levay <bryan@bryanlevay.com>
2020-09-13 19:27:44 -0700

    convenience const for count, add basic instructions

---
+++ README.md
@@ -0,0 +1,14 @@
+# Ember CSS perf test bed
+
+Contains three basic ember apps in `packages`.
+
+* `css-blocks-app` includes sass and eyeglass preprocessing
+* `css-modules-app` includes sass preprocessing
+* `plain-scss-app` uses `ember-cli-sass` and BEM style classes.
+
+
+## Usage
+
+* `yarn install` at the root
+* `yarn generate` at the root to create a bunch of components and corresponding styles. (edit the `COUNT` constant in `gen-content.js` if needed)
+* Do what you gotta do
--- gen-content.js
+++ gen-content.js
@@ -1,10 +1,15 @@
 const Proliferation = require('proliferate');
 
-const cssBlocksProliferation = new Proliferation(require('./proliferations/css-blocks'));
+const COUNT = 1000;
+
+const cssBlocksConfig = require('./proliferations/css-blocks');
+const cssBlocksProliferation = new Proliferation(Object.assign({}, cssBlocksConfig, { count: COUNT }));
 cssBlocksProliferation.write();
 
-const cssModulesProliferation = new Proliferation(require('./proliferations/css-modules'));
+const cssModulesConfig = require('./proliferations/css-modules');
+const cssModulesProliferation = new Proliferation(Object.assign({}, cssModulesConfig, { count: COUNT }));
 cssModulesProliferation.write();
 
-const plainSassProliferation = new Proliferation(require('./proliferations/plain-scss'));
+const plainSassConfig = require('./proliferations/plain-scss');
+const plainSassProliferation = new Proliferation(Object.assign({}, plainSassConfig, { count: COUNT }));
 plainSassProliferation.write();
